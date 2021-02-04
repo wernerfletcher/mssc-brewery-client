13df3422f2b4b29c8e1727d08c4607ecc395ef04
Werner Fletcher <wernerf@cibecs.com>
2021-02-04 21:00:24 +0200

    Externalize properties

--- src/main/java/guru/springframework/msscbreweryclient/web/config/BlockingRestTemplateCustomizer.java
+++ src/main/java/guru/springframework/msscbreweryclient/web/config/BlockingRestTemplateCustomizer.java
@@ -5,6 +5,7 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateCustomizer;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -14,14 +15,29 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class BlockingRestTemplateCustomizer implements RestTemplateCustomizer {
 
+    private final int connectionPoolMaxTotal;
+    private final int connectionPoolDefaultMaxPerRoute;
+    private final int connectionRequestTimeout;
+    private final int requestSocketTimeout;
+
+    public BlockingRestTemplateCustomizer(@Value("${rest.template.connectionPoolMaxTotal}") int connectionPoolMaxTotal,
+                                          @Value("${rest.template.connectionPoolDefaultMaxPerRoute}") int connectionPoolDefaultMaxPerRoute,
+                                          @Value("${rest.template.connectionRequestTimeout}") int connectionRequestTimeout,
+                                          @Value("${rest.template.requestSocketTimeout}") int requestSocketTimeout) {
+        this.connectionPoolMaxTotal = connectionPoolMaxTotal;
+        this.connectionPoolDefaultMaxPerRoute = connectionPoolDefaultMaxPerRoute;
+        this.connectionRequestTimeout = connectionRequestTimeout;
+        this.requestSocketTimeout = requestSocketTimeout;
+    }
+
     private ClientHttpRequestFactory requestFactory() {
         PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
-        connectionManager.setMaxTotal(100);
-        connectionManager.setDefaultMaxPerRoute(20);
+        connectionManager.setMaxTotal(connectionPoolMaxTotal);
+        connectionManager.setDefaultMaxPerRoute(connectionPoolDefaultMaxPerRoute);
 
         RequestConfig requestConfig = RequestConfig.custom()
-                .setConnectionRequestTimeout(5000)
-                .setSocketTimeout(5000)
+                .setConnectionRequestTimeout(connectionRequestTimeout)
+                .setSocketTimeout(requestSocketTimeout)
                 .build();
 
         CloseableHttpClient httpClient = HttpClients.custom()
